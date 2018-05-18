07f1efbd34526d6a1edce1b9acc12cbe84b5ffca
Robert Tabares <robtabares92@gmail.com>
2018-05-18 00:01:20 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,12 @@ def my_select(collection)
   if block_given?
     n = 0
     output = []
-    while n < collection.length
+    while n < collection.length do
       if yield(collection[n]) == true
         output << yield(collection[n])
         n += 1
+      else
+        return nil
       end
     end
   end
