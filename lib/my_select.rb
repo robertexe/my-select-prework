b3b7d891171ed894cc124eb478b4f7ec92f4a19d
Robert Tabares <robtabares92@gmail.com>
2018-05-18 00:10:54 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,7 @@ def my_select(collection)
     n = 0
     output = []
     while n < collection.length do
-      if yield(collection[n]) == true
+      if yield(collection[n])
         output << collection[n]
       end
       n+=1
