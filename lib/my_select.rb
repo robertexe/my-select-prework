8c2ea609ff0ca8d65eae3c6189bf1aae1a7c661d
Robert Tabares <robtabares92@gmail.com>
2018-05-18 00:11:37 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    n = 0
+    output = []
+    while n < collection.length do
+      if yield(collection[n])
+        output << collection[n]
+      end
+      n+=1
+    end
+    return output
+  end
+end
+
+
+my_select([1, 2, 3, 4, 5]) do |num|
+  if num.even? == true
+    num
+  end
 end
